5814476183128303d4010bad45be28f1830e9ab0
Ali <ali.boushehri@roche.com>
2020-08-21 14:46:47 +0200

    deleted pesto examples for compatibility

--- MATLAB/toolboxes/PESTO/PESTO/examples/HierarchicalOptimization_examples/RafMekErk/models/RafMekErk_hierarchical_syms.m
+++
@@ -1,9 +0,0 @@
-function [model] = RafMekErk_hierarchical_syms()
-
-model = RafMekErk_syms();
-     
-%% SYSTEM STRUCT
-model.sym.p = model.sym.p(1:12);
-model.sym.y = [model.sym.x(2),model.sym.x(3)];
-
-end
--- MATLAB/toolboxes/PESTO/PESTO/examples/HierarchicalOptimization_examples/RafMekErk/models/compilation_RafMekErk.m
+++
@@ -1,9 +0,0 @@
-%% compilation RafMekErk for standard
-[exdir,~,~]=fileparts(which('RafMekErk_syms.m'));
-% compile the model
-amiwrap('RafMekErk','RafMekErk_syms',exdir)
-
-%% compilation RafMekErk for hierarchical
-[exdir,~,~]=fileparts(which('RafMekErk_hierarchical_syms.m'));
-% compile the model
-amiwrap('RafMekErk_hierarchical','RafMekErk_hierarchical_syms',exdir)
